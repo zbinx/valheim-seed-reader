5103ee939b9dcb5d03629b6bdc7b5c16524d6524
h4ckery.b1nx <51971090+zbinx@users.noreply.github.com>
2022-05-10 09:18:43 -0400

    adding code via upload

---
+++ valSeed.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# a bash script to find out your valheim worlds and seeds
+
+echo Type [1] to see a list of your Valheim worlds.
+echo ..........................................
+echo Type [2] to see the seed of your world.
+
+read input
+if [[ $input == "1" ]]; then
+	ls -1 ~/.config/unity3d/IronGate/Valheim/worlds
+else
+	if [[ $input == "2" ]]; then
+		echo Which world seed? 
+		read input
+		echo **Seed is the 10 digits after your world name**
+		cat ~/.config/unity3d/IronGate/Valheim/worlds/$input.fwl
+		exit 1
+	fi
+fi
+
+
+
+
+
+
+
+
+
